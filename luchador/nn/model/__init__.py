7d7f258b6b3a42372afdfea8dd631fc9dfdf7eb4
moto (・_・) <mthrok@gmail.com>
2016-06-14 18:42:35 -0700

    Change model factory signature

--- luchador/nn/model/__init__.py
+++ luchador/nn/model/__init__.py
@@ -5,9 +5,9 @@ from .preprocessing import image_normalizer
 from .dqn import vanilla_dqn
 
 
-def model_factory(name):
+def model_factory(name, **kwargs):
     if name == 'image_normalizer':
-        return image_normalizer
+        return image_normalizer(**kwargs)
     elif name == 'vanilla_dqn':
-        return vanilla_dqn
+        return vanilla_dqn(**kwargs)
     raise ValueError('Unknown model name: {}'.format(name))
