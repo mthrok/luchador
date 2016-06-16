dede3b2c245f89cc59fa263bc4a9d94b9c9f9808
moto (・_・) <mthrok@gmail.com>
2016-06-16 16:48:39 -0700

    Fix DQN structure

--- luchador/nn/model/dqn.py
+++ luchador/nn/model/dqn.py
@@ -11,23 +11,21 @@ from ..core import Normal
 
 
 def vanilla_dqn(n_actions):
-
     initializers = {
         'bias': Normal(mean=0.0, stddev=0.01),
         'weight': Normal(mean=0.0, stddev=0.01),
     }
 
     conv0 = Conv2D(filter_height=8, filter_width=8, n_filters=32, strides=4,
-                   initializers=initializers, padding='SAME')
+                   initializers=initializers, padding='valid')
     conv1 = Conv2D(filter_height=4, filter_width=4, n_filters=64, strides=2,
-                   initializers=initializers)
+                   initializers=initializers, padding='valid')
     conv2 = Conv2D(filter_height=3, filter_width=3, n_filters=64, strides=1,
-                   initializers=initializers)
+                   initializers=initializers, padding='valid')
     dense0 = Dense(n_nodes=512, initializers=initializers)
     dense1 = Dense(n_nodes=n_actions, initializers=initializers)
 
     model = Model()
-    # Note: In original DQN, they have 1 pixel 0 padding for each dimention.
     model.add(conv0, scope='layer0/conv2D')
     model.add(ReLU(), scope='layer0/ReLU')
 
