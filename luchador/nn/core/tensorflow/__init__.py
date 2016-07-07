2fa00151f63f8d5b9977b5c6d246c832f990c860
moto (・_・) <mthrok@gmail.com>
2016-07-07 16:05:13 -0700

    Remove luchador/nn/core/tensorflow/optimization

--- luchador/nn/core/tensorflow/__init__.py
+++ luchador/nn/core/tensorflow/__init__.py
@@ -1,11 +1,13 @@
 from __future__ import absolute_import
 
 from .scope import *  # nopep8
+
+from .tensor import Tensor, Input  # nopep8
 from .layer import *  # nopep8
 from .model import Model  # nopep8
-from .tensor import Tensor, Input  # nopep8
+
 from .initializer import *  # nopep8
-from .optimization import *  # nopep8
+
 from .q_learning import QLearningInterface  # nopep8
 
 from .io import Saver  # nopep8
