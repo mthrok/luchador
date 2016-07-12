a97e5efe2ee161080bc08e96bbe15d1737e48912
moto (・_・) <mthrok@gmail.com>
2016-07-12 11:18:28 -0700

    Add stride check

--- luchador/nn/core/tensorflow/layer.py
+++ luchador/nn/core/tensorflow/layer.py
@@ -2,6 +2,7 @@ from __future__ import division
 from __future__ import absolute_import
 
 import logging
+import warnings
 
 import tensorflow as tf
 from tensorflow.contrib import layers
@@ -60,7 +61,7 @@ class Conv2D(BaseConv2D):
         try:
             if (
                     len(strides) in [2, 4] and
-                    all(map(lambda s: issubclass(s, int), strides))
+                    all(map(lambda s: isinstance(s, int), strides))
             ):
                 return
         except Exception:
@@ -90,17 +91,45 @@ class Conv2D(BaseConv2D):
         height, width = self.args['filter_height'], self.args['filter_width']
         return (height, width, n_in, n_out)
 
+    def _check_filter_shape(self, input_shape, filter_shape):
+        flt_h, flt_w = filter_shape[0], filter_shape[1]
+        strides = self._get_strides()
+        if CNN_FORMAT == 'NCHW':
+            img_h, img_w = input_shape[2], input_shape[3]
+            str_h, str_w = strides[2], strides[3]
+        else:
+            img_h, img_w = input_shape[1], input_shape[2]
+            str_h, str_w = strides[1], strides[2]
+        if self.args['padding'] == 'VALID':
+            warn_w = bool((img_w - flt_w) % str_w)
+            warn_h = bool((img_h - flt_h) % str_h)
+        else:
+            warn_w = bool((img_w - 1) % str_w)
+            warn_h = bool((img_h - 1) % str_h)
+        if warn_w:
+            warnings.warn(
+                'Convolution op will not cover the right side of the input.'
+                'Check the width configuration of filter and stride.',
+                RuntimeWarning
+            )
+        if warn_h:
+            warnings.warn(
+                'Convolution op will not cover the bottom part of the input.'
+                'Check the height configuration of filter and stride.',
+                RuntimeWarning
+            )
+
     def _instantiate_parameter_variables(self, input_shape):
         _LG.debug('    Input shape: {}'.format(input_shape))
         b_shape = (self.args['n_filters'],)
         w_shape = self._get_weight_shape(input_shape)
 
+        self._check_filter_shape(input_shape, w_shape)
+
         given = self.args.get('initializers')
         b0 = given['bias'] if given else tf.constant_initializer(0.1)
         w0 = given['weight'] if given else layers.xavier_initializer_conv2d()
 
-        # TODO: Add warning if
-        # parts of image are not covered because of stride
         b = tf.get_variable(
             name='bias', shape=b_shape, initializer=b0, dtype=DTYPE)
         w = tf.get_variable(
