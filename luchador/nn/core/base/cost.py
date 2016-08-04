afe92ffa31caaa0497b25114e66c3102604b520d
moto (・_・) <mthrok@gmail.com>
2016-08-04 11:25:04 -0700

    Add docstrings

--- luchador/nn/core/base/cost.py
+++ luchador/nn/core/base/cost.py
@@ -11,11 +11,14 @@ _LG = logging.getLogger(__name__)
 
 
 class BaseCost(CopyMixin, object):
+    """Base class for cost computation"""
     def __init__(self, args):
+        """Validate args and set it as instance property. See CopyMixin"""
         super(BaseCost, self).__init__()
         self._store_args(args)
 
     def __call__(self, target, prediction):
+        """Build cost between target and prediction"""
         return self.build(target, prediction)
 
     def build(self, target, prediction):
--- luchador/nn/core/base/layer.py
+++ luchador/nn/core/base/layer.py
@@ -12,26 +12,29 @@ __all__ = ['Dense', 'Conv2D', 'ReLU', 'Flatten', 'TrueDiv']
 
 
 class CopyMixin(object):
+    """Provide copy method which creates a copy of initialed instance"""
     def _store_args(self, args):
         """
         Args:
-            args (dict): Arguments used to initialize the subclass instance.
+            args (dict): Arguments passed to __init__ of the subclass instance.
             This will be used to recreate a subclass instance.
         """
         self._validate_args(args)
         self.args = args
 
     def _validate_args(self, args):
-        """Perform parameter validation"""
+        """Validate arguments"""
         pass
 
     def copy(self):
-        """Create new layer instance with the same configuration"""
+        """Create and initialize new instance with the same argument"""
         return type(self)(**self.args)
 
 
 class BaseLayer(CopyMixin, object):
+    """Defines common interface (copy and build methods) for Layer classes"""
     def __init__(self, args):
+        """Validate and store arguments passed to subclass __init__ method"""
         super(BaseLayer, self).__init__()
         self._store_args(args)
         self.parameter_variables = OrderedDict()
@@ -51,22 +54,29 @@ class BaseLayer(CopyMixin, object):
 ###############################################################################
 class Dense(BaseLayer):
     def __init__(self, n_nodes, initializers=None):
+        """Initialize dense (also called affine, linear or inner product) layer.
+        Activation functio such as ReLU is not included.
+
+        Args:
+          n_nodes (int): The number of internal neurons.
+        """
         super(Dense, self).__init__(args=get_function_args())
 
 
 class Conv2D(BaseLayer):
     def __init__(self, filter_height, filter_width, n_filters, strides,
                  padding='VALID', initializers=None, **kwargs):
-        """
+        """Initialize 2D convolution layer.
         Args:
-          filter_height (int): filter height
-          filter_weight (int): filter weight
-          n_filters (int): #filters == #output channels
+          filter_height (int): filter height (== row)
+          filter_weight (int): filter weight (== column)
+          n_filters (int): #filters (== #output channels)
           strides (int, tuple of two int, or tuple of four int): stride
-            - When given type is int, the out put is subsampled by this factor
+            - When given type is int, the output is subsampled by this factor
               in both width and height direction.
-            - When given type is tuple of two int, subsapmled `strides[0]` in
-              height direction and `striders[1]` in width direction.
+            - When given type is tuple of two int, the output is subsapmled
+              `strides[0]` in height direction and `striders[1]` in width
+              direction.
             - [Tensorflow only] When given type is tuple of four int, it must
               be consistent with the input data format. That is:
               - data_format=='NHWC' (default): [batch, height, width, channel]
