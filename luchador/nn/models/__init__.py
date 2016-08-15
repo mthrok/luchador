2fd3c13e684d762270ce42f698714d31fe90c287
moto (・_・) <mthrok@gmail.com>
2016-08-15 14:43:01 -0700

    Move model factory to util

--- luchador/nn/models/__init__.py
+++ luchador/nn/models/__init__.py
@@ -1,13 +1,4 @@
 from __future__ import absolute_import
 
-from .preprocessing import image_normalizer
-
-from .dqn import vanilla_dqn
-
-
-def model_factory(name, **kwargs):
-    if name == 'image_normalizer':
-        return image_normalizer(**kwargs)
-    elif name == 'vanilla_dqn':
-        return vanilla_dqn(**kwargs)
-    raise ValueError('Unknown model name: {}'.format(name))
+from .preprocessing import *  # noqa: F401, F403
+from .dqn import *  # noqa: F401, F403
--- luchador/nn/util.py
+++ luchador/nn/util.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 import inspect
 
 import luchador.nn
+import luchador.nn.models
 
 
 def get_optimizer(name):
@@ -13,3 +14,11 @@ def get_optimizer(name):
         ):
             return obj[1]
     raise ValueError('Unknown Optimizer: {}'.format(name))
+
+
+def get_model(name, **kwargs):
+    for obj in inspect.getmembers(luchador.nn.models, inspect.isfunction):
+        if name == obj[0]:
+            model_maker_func = obj[1]
+            return model_maker_func(**kwargs)
+    raise ValueError('Unknown model name: {}'.format(name))
