b930b16475823102549203e4873d3f1ebbab0453
moto (・_・) <mthrok@gmail.com>
2016-08-17 19:30:25 -0700

    Fix test

--- tests/nn/core/theano/scope_test.py
+++ tests/nn/core/theano/scope_test.py
@@ -2,30 +2,20 @@ from __future__ import absolute_import
 
 import unittest
 
-from luchador.nn.core.theano.scope import (
-    _set_flag,
-    _get_flag,
-    _set_scope,
-    _get_scope,
-    get_variable,
-    VariableScope,
-    variable_scope,
-    get_variable_scope,
-)
+from luchador.nn.core.theano import scope as scp
 
 
 def _reset():
     """Reset global state of scope and related objects"""
-    import luchador.nn.core.theano.scope as scope_module
-    _set_flag(False)
-    _set_scope('')
-    scope_module._VARIABLES = {}
+    scp._set_flag(False)
+    scp._set_scope('')
+    scp._VARIABLES = {}
 
 
 class TestVariableScopeClass(unittest.TestCase):
     def tearDown(self):
         # After each test, scope should be reset to root
-        expected, found = '', _get_scope()
+        expected, found = '', scp._get_scope()
         # Sanitize global scope for the next test in case something went wrong
         _reset()
         self.assertEqual(
@@ -36,25 +26,25 @@ class TestVariableScopeClass(unittest.TestCase):
     def test_update_scope(self):
         """VariableScope updates current variable scope"""
         scopes = ['aaa', 'bbb', 'ccc']
-        with VariableScope(reuse=False, name=scopes[0]):
+        with scp.VariableScope(reuse=False, name=scopes[0]):
             expected = scopes[0]
-            found = _get_scope()
+            found = scp._get_scope()
             self.assertEqual(
                 expected, found,
                 'Failed to update current variable scope. '
                 'Expected: "{}", Found: "{}"'.format(expected, found)
             )
-            with VariableScope(reuse=False, name=scopes[1]):
+            with scp.VariableScope(reuse=False, name=scopes[1]):
                 expected = scopes[1]
-                found = _get_scope()
+                found = scp._get_scope()
                 self.assertEqual(
                     expected, found,
                     'Failed to update current variable scope. '
                     'Expected: "{}", Found: "{}"'.format(expected, found)
                 )
-                with VariableScope(reuse=False, name=scopes[2]):
+                with scp.VariableScope(reuse=False, name=scopes[2]):
                     expected = scopes[2]
-                    found = _get_scope()
+                    found = scp._get_scope()
                     self.assertEqual(
                         expected, found,
                         'Failed to update current variable scope. '
@@ -63,49 +53,49 @@ class TestVariableScopeClass(unittest.TestCase):
 
     def test_update_reuse_flag(self):
         """VariableScope updates current reuse flag"""
-        with VariableScope(reuse=False, name='scope'):
+        with scp.VariableScope(reuse=False, name='scope'):
             expected = False
-            found = _get_flag()
+            found = scp._get_flag()
             self.assertEqual(
                 expected, found,
                 'Failed to update current reuse flag. '
                 'Expected: "{}", Found: "{}"'.format(expected, found)
             )
-            with VariableScope(reuse=False, name='scope'):
+            with scp.VariableScope(reuse=False, name='scope'):
                 expected = False
-                found = _get_flag()
+                found = scp._get_flag()
                 self.assertEqual(
                     expected, found,
                     'Failed to update current reuse flag. '
                     'Expected: "{}", Found: "{}"'.format(expected, found)
                 )
-            with VariableScope(reuse=True, name='scope'):
+            with scp.VariableScope(reuse=True, name='scope'):
                 expected = True
-                found = _get_flag()
+                found = scp._get_flag()
                 self.assertEqual(
                     expected, found,
                     'Failed to update current reuse flag. '
                     'Expected: "{}", Found: "{}"'.format(expected, found)
                 )
-        with VariableScope(reuse=True, name='scope'):
+        with scp.VariableScope(reuse=True, name='scope'):
             expected = True
-            found = _get_flag()
+            found = scp._get_flag()
             self.assertEqual(
                 expected, found,
                 'Failed to update current reuse flag. '
                 'Expected: "{}", Found: "{}"'.format(expected, found)
             )
-            with VariableScope(reuse=False, name='scope'):
+            with scp.VariableScope(reuse=False, name='scope'):
                 expected = False
-                found = _get_flag()
+                found = scp._get_flag()
                 self.assertEqual(
                     expected, found,
                     'Failed to update current reuse flag. '
                     'Expected: "{}", Found: "{}"'.format(expected, found)
                 )
-            with VariableScope(reuse=True, name='scope'):
+            with scp.VariableScope(reuse=True, name='scope'):
                 expected = True
-                found = _get_flag()
+                found = scp._get_flag()
                 self.assertEqual(
                     expected, found,
                     'Failed to update current reuse flag. '
@@ -115,13 +105,13 @@ class TestVariableScopeClass(unittest.TestCase):
     def test_reusing_VS_object_correctly_reset_scope(self):
         """Reusing VariableScope changes scope and flag correctly"""
         scope, flag = 'aaa', False
-        with VariableScope(reuse=flag, name=scope) as vs:
+        with scp.VariableScope(reuse=flag, name=scope) as vs:
             pass
 
-        with VariableScope(True, 'scope_to_be_ignored'):
+        with scp.VariableScope(True, 'scope_to_be_ignored'):
             with vs:
                 expected = scope
-                found = _get_scope()
+                found = scp._get_scope()
                 self.assertEqual(
                     expected, found,
                     'Failed to update current variable scope '
@@ -129,7 +119,7 @@ class TestVariableScopeClass(unittest.TestCase):
                     'Expected: "{}", Found: "{}"'.format(expected, found)
                 )
                 expected = flag
-                found = _get_flag()
+                found = scp._get_flag()
                 self.assertEqual(
                     expected, found,
                     'Failed to update current reuse flag '
@@ -141,12 +131,12 @@ class TestVariableScopeClass(unittest.TestCase):
         """VariableScope revert current variable scope after close"""
         scopes = ['aaa', 'bbb', 'ccc']
         for scope in scopes:
-            pre_scope = _get_scope()
-            pre_reuse = _get_flag()
-            with VariableScope(False, scope):
+            pre_scope = scp._get_scope()
+            pre_reuse = scp._get_flag()
+            with scp.VariableScope(False, scope):
                 pass
-            post_scope = _get_scope()
-            post_reuse = _get_flag()
+            post_scope = scp._get_scope()
+            post_reuse = scp._get_flag()
 
             expected = pre_scope
             found = post_scope
@@ -166,11 +156,11 @@ class TestVariableScopeClass(unittest.TestCase):
 
     def test_reuse_variables_enables_flag(self):
         """reuse_variables call enable reuse flag"""
-        with VariableScope(reuse=False, name='base_scope'):
-            with VariableScope(reuse=False, name='base') as vs:
+        with scp.VariableScope(reuse=False, name='base_scope'):
+            with scp.VariableScope(reuse=False, name='base') as vs:
                 vs.reuse_variables()
                 expected = True
-                found = _get_flag()
+                found = scp._get_flag()
                 self.assertEqual(
                     expected, found,
                     'Failed to update current variable scope '
@@ -180,22 +170,22 @@ class TestVariableScopeClass(unittest.TestCase):
 
     def test_flag_reset_after_reuse_variables(self):
         """reuse_variables call enable reuse flag"""
-        with VariableScope(reuse=False, name='base_scope'):
-            with VariableScope(reuse=False, name='base') as vs:
+        with scp.VariableScope(reuse=False, name='base_scope'):
+            with scp.VariableScope(reuse=False, name='base') as vs:
                 vs.reuse_variables()
             expected = False
-            found = _get_flag()
+            found = scp._get_flag()
             self.assertEqual(
                 expected, found,
                 'Failed to reset the flag after calling reuse_variables'
                 'Expected: "{}", Found: "{}"'.format(expected, found)
             )
 
-        with VariableScope(reuse=True, name='base_scope'):
-            with VariableScope(reuse=False, name='base') as vs:
+        with scp.VariableScope(reuse=True, name='base_scope'):
+            with scp.VariableScope(reuse=False, name='base') as vs:
                 vs.reuse_variables()
             expected = True
-            found = _get_flag()
+            found = scp._get_flag()
             self.assertEqual(
                 expected, found,
                 'Failed to reset the flag after calling reuse_variables'
@@ -206,7 +196,7 @@ class TestVariableScopeClass(unittest.TestCase):
 class TestVariableScopeFuncs(unittest.TestCase):
     def tearDown(self):
         # After each test, scope should be reset to root
-        expected, found = '', _get_scope()
+        expected, found = '', scp._get_scope()
         # Sanitize global scope for the next test in case something went wrong
         _reset()
         self.assertEqual(
@@ -217,33 +207,33 @@ class TestVariableScopeFuncs(unittest.TestCase):
     def test_variable_scope(self):
         """variable_scope stacks new scope on the current scope"""
         scopes = ['aaa', 'bbb', 'ccc']
-        with variable_scope(scopes[0]):
+        with scp.variable_scope(scopes[0]):
             expected = scopes[0]
-            found = _get_scope()
+            found = scp._get_scope()
             self.assertEqual(
                 expected, found,
                 'Failed to update current variable scope with variable_scope'
                 'Expected: "{}", Found: "{}"'.format(expected, found)
             )
-            with variable_scope(scopes[1]):
+            with scp.variable_scope(scopes[1]):
                 expected = '/'.join(scopes[:2])
-                found = _get_scope()
+                found = scp._get_scope()
                 self.assertEqual(
                     expected, found,
                     'Failed to stack variable scopes with variable_scope'
                     'Expected: "{}", Found: "{}"'.format(expected, found)
                 )
-                with variable_scope(scopes[2]):
+                with scp.variable_scope(scopes[2]):
                     expected = '/'.join(scopes[:3])
-                    found = _get_scope()
+                    found = scp._get_scope()
                     self.assertEqual(
                         expected, found,
                         'Failed to stack variable scopes with variable_scope'
                         'Expected: "{}", Found: "{}"'.format(expected, found)
                     )
-            with variable_scope(scopes[2]):
+            with scp.variable_scope(scopes[2]):
                 expected = '/'.join([scopes[0], scopes[2]])
-                found = _get_scope()
+                found = scp._get_scope()
                 self.assertEqual(
                     expected, found,
                     'Failed to stack variable scopes with variable_scope'
@@ -253,8 +243,8 @@ class TestVariableScopeFuncs(unittest.TestCase):
     def test_get_variable_scope(self):
         """get_variable_scope retrieves the current scope and reuse flag"""
         scope, reuse = 'aaa', False
-        with variable_scope(scope, reuse=reuse):
-            vs = get_variable_scope()
+        with scp.variable_scope(scope, reuse=reuse):
+            vs = scp.get_variable_scope()
 
             expected = scope
             found = vs.name
@@ -273,8 +263,8 @@ class TestVariableScopeFuncs(unittest.TestCase):
             )
 
         scope, reuse = 'aaa', True
-        with variable_scope(scope, reuse=reuse):
-            vs = get_variable_scope()
+        with scp.variable_scope(scope, reuse=reuse):
+            vs = scp.get_variable_scope()
 
             expected = scope
             found = vs.name
@@ -293,9 +283,9 @@ class TestVariableScopeFuncs(unittest.TestCase):
             )
 
         scopes, reuses = ['aaa', 'bbb'], [False, False]
-        with variable_scope(scopes[0], reuse=reuses[0]):
-            with variable_scope(scopes[1], reuse=reuses[1]):
-                vs = get_variable_scope()
+        with scp.variable_scope(scopes[0], reuse=reuses[0]):
+            with scp.variable_scope(scopes[1], reuse=reuses[1]):
+                vs = scp.get_variable_scope()
 
                 expected = '/'.join(scopes)
                 found = vs.name
@@ -315,9 +305,9 @@ class TestVariableScopeFuncs(unittest.TestCase):
                 )
 
         scopes, reuses = ['aaa', 'bbb'], [False, True]
-        with variable_scope(scopes[0], reuse=reuses[0]):
-            with variable_scope(scopes[1], reuse=reuses[1]):
-                vs = get_variable_scope()
+        with scp.variable_scope(scopes[0], reuse=reuses[0]):
+            with scp.variable_scope(scopes[1], reuse=reuses[1]):
+                vs = scp.get_variable_scope()
 
                 expected = '/'.join(scopes)
                 found = vs.name
@@ -337,9 +327,9 @@ class TestVariableScopeFuncs(unittest.TestCase):
                 )
 
         scopes, reuses = ['aaa', 'bbb'], [True, True]
-        with variable_scope(scopes[0], reuse=reuses[0]):
-            with variable_scope(scopes[1], reuse=reuses[1]):
-                vs = get_variable_scope()
+        with scp.variable_scope(scopes[0], reuse=reuses[0]):
+            with scp.variable_scope(scopes[1], reuse=reuses[1]):
+                vs = scp.get_variable_scope()
 
                 expected = '/'.join(scopes)
                 found = vs.name
@@ -359,9 +349,9 @@ class TestVariableScopeFuncs(unittest.TestCase):
                 )
 
         scopes, reuses = ['aaa', 'bbb'], [True, False]
-        with variable_scope(scopes[0], reuse=reuses[0]):
-            with variable_scope(scopes[1], reuse=reuses[1]):
-                vs = get_variable_scope()
+        with scp.variable_scope(scopes[0], reuse=reuses[0]):
+            with scp.variable_scope(scopes[1], reuse=reuses[1]):
+                vs = scp.get_variable_scope()
 
                 expected = '/'.join(scopes)
                 found = vs.name
@@ -384,7 +374,7 @@ class TestVariableScopeFuncs(unittest.TestCase):
 class TestGetVariable(unittest.TestCase):
     def tearDown(self):
         # After each test, scope should be reset to root
-        expected, found = '', _get_scope()
+        expected, found = '', scp._get_scope()
         # Sanitize global scope for the next test in case something went wrong
         _reset()
         self.assertEqual(
@@ -394,23 +384,23 @@ class TestGetVariable(unittest.TestCase):
 
     def test_get_variable_creates_variable(self):
         """get_variable create variable"""
-        get_variable('test_var', shape=[3, 1])
+        scp.get_variable('test_var', shape=[3, 1])
 
     def test_get_variable_reuse_variable(self):
         """get_variable create variable"""
         name = 'test_var'
-        var1 = get_variable(name, shape=[3, 1])
-        _set_flag(True)
-        var2 = get_variable(name)
+        var1 = scp.get_variable(name, shape=[3, 1])
+        scp._set_flag(True)
+        var2 = scp.get_variable(name)
         self.assertIs(
             var1.get(), var2.get(),
             'Reused variable should be identical to the original variable')
 
     def test_get_variable_raises_when_reuseing_non_existent_variable(self):
         """get_variable raise when trying to reuse non existent variable"""
-        _set_flag(True)
+        scp._set_flag(True)
         try:
-            get_variable('non_existent_var')
+            scp.get_variable('non_existent_var')
         except ValueError:
             pass
         else:
@@ -420,9 +410,9 @@ class TestGetVariable(unittest.TestCase):
     def test_get_variable_raises_when_creating_already_existing_variable(self):
         """get_variable raise when trying to create existent variable"""
         name = 'aaa'
-        get_variable(name, shape=[3, 1])
+        scp.get_variable(name, shape=[3, 1])
         try:
-            get_variable(name)
+            scp.get_variable(name)
         except ValueError:
             pass
         else:
