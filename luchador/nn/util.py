856029164b9dcf3e7d10ab7fe9898e2320fdd4d1
moto (・_・) <mthrok@gmail.com>
2016-08-18 22:44:06 -0700

    Add get_initializer function

--- luchador/nn/util.py
+++ luchador/nn/util.py
@@ -6,7 +6,19 @@ import luchador.nn
 import luchador.nn.models
 
 
+def get_initializer(name):
+    # TODO: Convert this to return instance
+    for name_, class_ in inspect.getmembers(luchador.nn, inspect.isclass):
+        if (
+                name == name_ and
+                issubclass(class_, luchador.nn.base.Initializer)
+        ):
+            return class_
+    raise ValueError('Unknown Optimizer: {}'.format(name))
+
+
 def get_optimizer(name):
+    # TODO: Convert this to return instance
     for name_, class_ in inspect.getmembers(luchador.nn, inspect.isclass):
         if (
                 name == name_ and
