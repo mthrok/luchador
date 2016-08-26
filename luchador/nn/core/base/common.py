32f4075bd73d8112c5eff2d843a731987a986cf8
moto (・_・) <mthrok@gmail.com>
2016-08-26 11:16:59 -0700

    Clean up CopyMixin code

--- luchador/nn/core/base/common.py
+++ luchador/nn/core/base/common.py
@@ -3,40 +3,65 @@ from __future__ import absolute_import
 
 class CopyMixin(object):
     """Provide copy method which creates a copy of initialed instance"""
-    ###########################################################################
-    # Argument checkers
-    def _store_args(self, args):
+    def copy(self):
+        """Create and initialize new instance with the same argument
+
+        Typical usage is
+        ```
+        class Foo(CopyMixin, object):
+            def __init__(self, arg1, arg2):
+                self._store_args({arg1: arg1, arg2: arg2})
+
+            def _validate_args(self, args):
+                if not isinstance(args['arg1'], int):
+                    raise ValueError('`arg1` must be int.')
+
+        obj1 = Foo(arg1=1, arg2=None)
+        obj2 = obj1.copy()  # object initialized with the
+                            # same constructor argument as obj1
+        ```
         """
+        return type(self)(**self.args)
+
+    def _store_args(self, args):
+        """Store initializer arguments to args
+
+        See `copy` method for detail
+
         Args:
-            args (dict): Arguments passed to __init__ of the subclass instance.
+            args (dict): Arguments passed to __init__.
             This will be used to recreate a subclass instance.
+
         """
         self._validate_args(args)
         self.args = args
 
     def _validate_args(self, args):
-        """Validate arguments"""
+        """Validate arguments
+
+        See `copy` method for detail
+
+        Args:
+            args (dict): Arguments passed to __init__.
+        """
         pass
 
     ###########################################################################
-    def copy(self):
-        """Create and initialize new instance with the same argument"""
-        return type(self)(**self.args)
-
-    ###########################################################################
-    # Configuration equality
+    # Shallow equality
     def __eq__(self, other):
-        return isinstance(other, self.__class__) and self.args == other.args
+        if isinstance(other, self.__class__):
+            return self.args == other.args
+        return NotImplemented
 
     def __ne__(self, other):
         return not self.__eq__(other)
 
     ###########################################################################
-    # IO
     def export(self):
+        """Export object configuration (constructor arguments) to dictionary"""
         args = {}
         for key, value in self.args.items():
-            args[key] = value
+            args[key] = value.export() if hasattr(value, 'export') else value
         return {
             'name': self.__class__.__name__,
             'args': args
