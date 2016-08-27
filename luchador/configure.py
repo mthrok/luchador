968d5e69279720931bc1066b2acd2a9d82326cbd
moto (・_・) <mthrok@gmail.com>
2016-08-26 17:10:26 -0700

    Add backend config setter

--- luchador/configure.py
+++ luchador/configure.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 import os
+import warnings
 
 _NN_DTYPE = os.environ.get('LUCHADOR_NN_DTYPE', 'float32')
 _NN_BACKEND = os.environ.get('LUCHADOR_NN_BACKEND', 'tensorflow')
@@ -8,7 +9,8 @@ _NN_CONV_FORMAT = os.environ.get('LUCHADOR_NN_CONV_FORMAT', 'NCHW')
 
 __all__ = [
     'get_nn_backend', 'get_nn_conv_format', 'get_nn_dtype',
- ]
+    'set_nn_backend', 'set_nn_conv_format', 'set_nn_dtype',
+]
 
 
 def get_nn_backend():
@@ -21,3 +23,26 @@ def get_nn_conv_format():
 
 def get_nn_dtype():
     return _NN_DTYPE
+
+
+def set_nn_backend(backend):
+    global _NN_BACKEND
+    _NN_BACKEND = backend
+
+
+def set_nn_conv_format(fmt):
+    if fmt not in ('NCHW', 'NHWC'):
+        raise ValueError('Convolution format must b either "NCHW" or "NHWC"')
+
+    if _NN_BACKEND == 'theano':
+        warnings.warn('Convolution format only affects "tensorflow" backend')
+
+    global _NN_CONV_FORMAT
+    _NN_CONV_FORMAT = fmt
+
+
+def set_nn_dtype(dtype):
+    global _NN_DTYPE
+
+    # TODO: Add validation
+    _NN_DTYPE = dtype
--- luchador/nn/core/__init__.py
+++ luchador/nn/core/__init__.py
@@ -1,6 +1,12 @@
 from __future__ import absolute_import
 
+import logging
+
 import luchador
+
+
+_LG = logging.getLogger(__name__)
+_LG.info('Using {} backend'.format(luchador.get_nn_backend()))
 
 if luchador.get_nn_backend() == 'tensorflow':
     from .tensorflow import *  # noqa: F401, F403
