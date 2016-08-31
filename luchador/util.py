02a285b89eec5490782658ef6bd055dd8f18d3e9
moto (・_・) <mthrok@gmail.com>
2016-08-30 17:30:39 -0700

    Add get_agent, get_env utilities

--- luchador/util.py
+++ luchador/util.py
@@ -1,8 +1,10 @@
 from __future__ import absolute_import
 
 import yaml
+import inspect
+import luchador
 
-__all__ = ['sane_gym_import', 'load_config']
+__all__ = ['sane_gym_import', 'load_config', 'get_env', 'get_agent']
 
 
 def sane_gym_import():
@@ -22,3 +24,23 @@ def sane_gym_import():
 def load_config(filepath):
     with open(filepath) as f:
         return yaml.load(f)
+
+
+def get_agent(name):
+    for name_, Class in inspect.getmembers(luchador, inspect.isclass):
+        if (
+                name == name_ and
+                issubclass(Class, luchador.agent.base.Agent)
+        ):
+            return Class
+    raise ValueError('Unknown Agent: {}'.format(name))
+
+
+def get_env(name):
+    for name_, Class in inspect.getmembers(luchador, inspect.isclass):
+        if (
+                name == name_ and
+                issubclass(Class, luchador.env.base.Environment)
+        ):
+            return Class
+    raise ValueError('Unknown Environment: {}'.format(name))
