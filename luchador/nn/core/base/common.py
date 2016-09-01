9436aca772baf62955dc9bfd2727de7c7c70e1bb
moto (・_・) <mthrok@gmail.com>
2016-09-01 10:12:00 -0700

    Add comment

--- luchador/nn/core/base/common.py
+++ luchador/nn/core/base/common.py
@@ -2,8 +2,17 @@ from __future__ import absolute_import
 
 
 def get_subclasses(Class):
+    """Get the list of all subclasses
+
+    This function is intended to use in get_layer, get_optimizer,
+    get_initializer for flexible model build.
+    """
     ret = []
     for SubClass in Class.__subclasses__():
+        # As we adopt subclassing-base-interface approach to realize backend
+        # switch, we prefer to return subclasses in the order similar to
+        # python's inheritence resolution.
+        # So the descendant classes comes before their anscestors.
         ret.extend(get_subclasses(SubClass))
         ret.append(SubClass)
     return ret
