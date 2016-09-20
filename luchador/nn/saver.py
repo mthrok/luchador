749bdcb663585efe7c9a192c33990e0a4ed459c8
moto (・_・) <mthrok@gmail.com>
2016-09-20 09:49:43 -0700

    Update saver debug message

--- luchador/nn/saver.py
+++ luchador/nn/saver.py
@@ -27,7 +27,8 @@ class Saver(object):
     def save(self, data):
         f = h5py.File(self.filepath, 'a')
         for key, value in data.items():
-            _LG.debug('Saving: {} {} {}'.format(key, value.dtype, value.shape))
+            _LG.debug('Saving: {:10} {:24} {}'.format(
+                value.dtype, value.shape, key))
             if key in f:
                 del f[key]
             f.create_dataset(key, data=value, chunks=True)
