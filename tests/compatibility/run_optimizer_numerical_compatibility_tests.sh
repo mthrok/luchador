cf11eb2fe53b4270e3ad65732008c6b278624e44
moto <mthrok@gmail.com>
2016-09-23 11:14:42 -0700

    Move test script (#3)
    Move test script one directory up so that it is easier to see on `circle.yml`

--- tests/compatibility/run_optimizer_numerical_compatibility_tests.sh
+++ tests/compatibility/run_optimizer_numerical_compatibility_tests.sh
@@ -2,20 +2,22 @@
 set -e
 
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TEST_DIR="${BASE_DIR}/test_optimizer_numerical_compatibility"
 
 # Get the list of optimizers in optimizer directory
 OPTIMIZERS=()
-for FILE in $(dirname ${BASE_DIR})/optimizer/*.yml
+for FILE in ${BASE_DIR}/optimizer/*.yml
 do
     OPTIMIZERS+=(${FILE})
 done
 
-FORMULAE="$(python ${BASE_DIR}/formula.py)"
+
+FORMULAE="$(python ${TEST_DIR}/formula.py)"
 for FORMULA in ${FORMULAE}
 do
     for OPTIMIZER in "${OPTIMIZERS[@]}"
     do
         echo "${FORMULA} - ${OPTIMIZER}"
-        ${BASE_DIR}/test_optimizer_compatibility.sh --optimizer ${OPTIMIZER} --formula ${FORMULA}
+        ${TEST_DIR}/test_optimizer_compatibility.sh --optimizer ${OPTIMIZER} --formula ${FORMULA}
     done
 done
