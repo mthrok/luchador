f181d17a52d5eb83350b5c02f730092482ca3420
moto <mthrok@gmail.com>
2016-09-27 16:46:45 -0700

    Organize artifacts created by unit test (#15)

--- tests/unit/nn/saver_test.py
+++ tests/unit/nn/saver_test.py
@@ -8,12 +8,16 @@ import numpy as np
 
 from luchador.nn.saver import Saver
 
-OUTPUT_DIR = 'tmp'
+OUTPUT_DIR = os.path.join('tmp', 'saver_test')
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
 
 
 def list_files(prefix, dir_name=OUTPUT_DIR):
-    return [os.path.join(dir_name, f) for f in os.listdir(dir_name)
-            if f.startswith(prefix)]
+    if not os.path.exists(dir_name):
+        return []
+    return [os.path.join(dir_name, f)
+            for f in os.listdir(dir_name) if f.startswith(prefix)]
 
 
 def remove_files(prefix, dir_name=OUTPUT_DIR):
@@ -29,8 +33,10 @@ class SaverTest(unittest.TestCase):
     def test_save(self):
         """`save` function can save data"""
         prefix = 'test_save'
-        remove_files(prefix)
-        saver = Saver(OUTPUT_DIR, prefix=prefix)
+
+        output_dir = os.path.join(OUTPUT_DIR, 'test_save')
+        remove_files(prefix, output_dir)
+        saver = Saver(output_dir, prefix=prefix)
 
         key = 'test'
         value1 = np.random.randint(low=0, high=2, size=(32, 8, 8, 4))
@@ -46,8 +52,10 @@ class SaverTest(unittest.TestCase):
     def test_append(self):
         """`save` function appends to existing file"""
         prefix, global_step = 'test_append', 0
-        remove_files(prefix)
-        saver = Saver(OUTPUT_DIR, prefix=prefix)
+
+        output_dir = os.path.join(OUTPUT_DIR, 'test_append')
+        remove_files(prefix, output_dir)
+        saver = Saver(output_dir, prefix=prefix)
 
         key1 = 'key1'
         value1 = gen_random_value()
@@ -76,8 +84,10 @@ class SaverTest(unittest.TestCase):
     def test_overwrite(self):
         """`save` function raises error if key exists"""
         prefix, global_step = 'test_overwrite', 0
-        remove_files(prefix)
-        saver = Saver(OUTPUT_DIR, prefix=prefix)
+
+        output_dir = os.path.join(OUTPUT_DIR, 'test_overwrite')
+        remove_files(prefix, output_dir)
+        saver = Saver(output_dir, prefix=prefix)
 
         key = 'foo'
         value1 = gen_random_value()
@@ -99,19 +109,24 @@ class SaverTest(unittest.TestCase):
     def test_max_to_keep(self):
         """Saver only keeps lates files"""
         prefix, max_to_keep = 'test_max_to_keep', 10
-        remove_files(prefix)
-        saver = Saver(OUTPUT_DIR, prefix=prefix, max_to_keep=max_to_keep)
+
+        output_dir = os.path.join(OUTPUT_DIR, 'test_max_to_keep')
+        remove_files(prefix, output_dir)
+        saver = Saver(output_dir, prefix=prefix, max_to_keep=max_to_keep)
 
         for step in range(2 * max_to_keep):
             saver.save({'foo': gen_random_value()}, global_step=step)
-            files_kept = len(list_files(prefix))
+            files_kept = len(list_files(prefix, output_dir))
             self.assertLessEqual(files_kept, max_to_keep)
 
     def test_keep_every_n_hours(self):
         prefix, max_to_keep = 'test_keep_every_n_hours', 10
         keep_every_n_hours = 1.0
-        remove_files(prefix)
-        saver = Saver(OUTPUT_DIR, prefix=prefix, max_to_keep=max_to_keep)
+
+        output_dir = os.path.join(OUTPUT_DIR, 'test_keep_every_n_hours')
+        remove_files(prefix, output_dir)
+        saver = Saver(output_dir, prefix=prefix, max_to_keep=max_to_keep,
+                      keep_every_n_hours=keep_every_n_hours)
 
         for step in range(max_to_keep + 1):
             saver.save({'foo': gen_random_value()}, global_step=step)
@@ -120,5 +135,5 @@ class SaverTest(unittest.TestCase):
             saver.last_saved -= 3600 * keep_every_n_hours
             for step in range(max_to_keep * (i+1), max_to_keep * (i + 2)):
                 saver.save({'foo': gen_random_value()}, global_step=step+1)
-                files_kept = len(list_files(prefix))
+                files_kept = len(list_files(prefix, output_dir))
                 self.assertEqual(files_kept, max_to_keep + i + 1)
