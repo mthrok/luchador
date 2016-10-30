5a96d6c79401d2f1bd93c9e57e7046dfc482d74e
moto <mthrok@gmail.com>
2016-10-30 15:47:19 -0700

    Build docs and push to gh-pages (#74)
    * Build docs and push to gh-pages

---
+++ cci_script/build_docs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -eu
+
+sphinx-apidoc -e -o docs/source/API luchador --force
+make -C docs html
+
+if [[ ${CIRCLECI:=false} = true ]]; then
+    git config user.email "mthrok@gmail.com"
+    git config user.name "CCI"
+    COMMIT="$( git log --pretty=oneline -1 )"
+
+    # Clean up the current repository so that we can change to gh-pages branch
+    git reset --hard
+
+    # Update remote branch
+    git fetch
+    git checkout gh-pages
+    git reset --hard origin/gh-pages
+
+    # Update html dir
+    rm -rf html
+    mv docs/build/html ./
+    git add ./html
+
+    # Update remote branch
+    git commit --allow-empty -m "[skip ci] ${COMMIT}"
+    git push
+fi
