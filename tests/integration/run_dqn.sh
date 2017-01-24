64de2e8b534f337c524478fad15b37c916edfc20
moto <mthrok@gmail.com>
2017-01-24 10:42:45 -0800

    Fix coverage report from run_dqn test (#121)

--- tests/integration/run_dqn.sh
+++ tests/integration/run_dqn.sh
@@ -5,9 +5,9 @@ BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DATA_DIR="${BASE_DIR}/data/dqn"
 
 if [ "${COUNT_INTEGRATION_COVERAGE:-false}" = true ]; then
-    TEST_COMMAND="coverage run --source luchador -a tool/profile.py"
+    TEST_COMMAND="coverage run --parallel-mode tool/profile.py"
 else
     TEST_COMMAND="luchador"
 fi
 
-${TEST_COMMAND} exercise "${DATA_DIR}/ALEEnvironment_train.yml" --agent "${DATA_DIR}/DQNAgent_train.yml" --episodes 20 --steps 500
+${TEST_COMMAND} exercise "${DATA_DIR}/ALEEnvironment_train.yml" --agent "${DATA_DIR}/DQNAgent_train.yml" --episodes 10 --steps 500
