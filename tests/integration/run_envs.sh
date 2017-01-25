7b14d78c642c2af455f5a605783a0968286e9c34
moto <mthrok@gmail.com>
2017-01-24 19:51:12 -0800

    Add integration test for other envs (#126)
    * Adding environment tests
    
    * Add cartpole to test

---
+++ tests/integration/run_envs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eux
+
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DATA_DIR="${BASE_DIR}/data/env"
+
+if [ "${COUNT_INTEGRATION_COVERAGE:-false}" = true ]; then
+    TEST_COMMAND="coverage run --parallel-mode tool/profile.py"
+else
+    TEST_COMMAND="luchador"
+fi
+
+RETURN=0
+for dir in ${DATA_DIR}/*/
+do
+    echo "${dir}agent.yml"
+    ${TEST_COMMAND} exercise "${dir}env.yml" --agent "${dir}agent.yml" --episodes 10 --steps 500
+    if [[ ! $? = 0 ]]; then
+	RETURN=1
+    fi
+done
+
+exit ${RETURN}
