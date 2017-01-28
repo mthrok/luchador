3f1d29b3dac7051a68043bd024358596710b09b6
moto <mthrok@gmail.com>
2017-01-27 17:36:38 -0800

    Add noise (#135)

--- luchador/agent/misc.py
+++ luchador/agent/misc.py
@@ -2,6 +2,7 @@
 from __future__ import division
 from __future__ import absolute_import
 
+import numpy as np
 from numpy.random import RandomState
 
 
@@ -52,3 +53,90 @@ class EGreedy(object):
         ret = self._rng.rand() < self.epsilon
         self.count += 1
         return ret
+
+
+class WienerNoise(object):
+    """Generate noise with Wiener process, i.e. gaussian noise
+
+    Parameters
+    ----------
+    shape : tuple
+        Shape of the noise to generate
+
+    delta, dt : float
+        Parameters to controll the process.
+        ``dt`` represents the time difference between calls to :func:`sample`
+        ``delta`` represents standard deviation
+
+        .. math::
+
+            x \\sim \\mathcal{N} (\\mu=0, \\sigma=delta^2 * dt)
+
+    seed
+        Seed for random generator
+    """
+    def __init__(self, shape, delta, dt, seed=None):
+        self.delta = delta
+        self.dt = dt
+        self.shape = shape
+
+        self._sigma = self.dt * (self.delta ** 2)
+        self._rng = RandomState(seed=seed)
+
+    def reset(self):
+        """Reset internal state. Do nothing for WienerNoise"""
+        pass
+
+    def sample(self):
+        """Sample noise
+
+        See :func:`WienerNoise` constructor for the value sampled.
+        """
+        return self._sigma * self._rng.randn(*self.shape)
+
+
+class OUNoise(WienerNoise):
+    """Generate temporally correlated noise with Ornstein-Uhlenbeck process
+
+    Parameters
+    ----------
+    shape : tuple
+        Shape of the noise to generate
+
+    mu, sigma, theta : float
+        Parameters to controll the process.
+
+        .. math::
+
+            x &\\leftarrow \\mu
+
+            dx &\\sim \\theta * (\\mu - x) +
+            \\sigma * \\mathcal{N} (\\mu=0, \\sigma=1)
+
+            x &\\leftarrow x + dx
+
+    seed
+        Seed for random generator
+    """
+    def __init__(self, shape, mu, sigma, theta, seed=None):
+        super(OUNoise, self).__init__(shape=shape, delta=1, dt=1, seed=seed)
+        self.mu = mu
+        self.sigma = sigma
+        self.theta = theta
+
+        self._state = None
+        self.reset()
+
+    def reset(self):
+        """Reset the internal state to :math:`mu`"""
+        self._state = self.mu * np.ones(self.shape)
+
+    def sample(self):
+        """Sample noise
+
+        See :func:`OUNoise` constructor for the value sampled.
+        """
+        base = super(OUNoise, self).sample()
+        diff = self.theta * (self.mu - self._state) + self.sigma * base
+        self._state += diff
+        return np.copy(self._state)
--- tests/unit/agent/misc_test.py
+++ tests/unit/agent/misc_test.py
@@ -1,16 +1,17 @@
 from __future__ import division
 from __future__ import absolute_import
 
+import os
 import unittest
 
-from luchador.agent.misc import EGreedy
+from luchador.agent import misc
 
 
 class EGreedyTest(unittest.TestCase):
     def test_anneal(self):
         """Epsilon decrease every time `act_random` is called"""
         e0, et, duration = 1.0, 0.1, 10
-        eg = EGreedy(epsilon_init=e0, epsilon_term=et, duration=duration)
+        eg = misc.EGreedy(epsilon_init=e0, epsilon_term=et, duration=duration)
         epsilon = e0
         self.assertTrue(eg.epsilon - epsilon < 1e-5)
         for _ in range(duration):
@@ -24,6 +25,49 @@ class EGreedyTest(unittest.TestCase):
     def test_anneal_0(self):
         """Epsilon is always e_term if dutaion=0"""
         et = 0.1
-        eg = EGreedy(epsilon_init=1.0, epsilon_term=et, duration=0)
+        eg = misc.EGreedy(epsilon_init=1.0, epsilon_term=et, duration=0)
         for _ in range(100):
             self.assertEqual(eg.epsilon, et)
+
+
+def _run_plot_test():
+    return 'TEST_NOISE' in os.environ
+
+
+class NoiseTest(unittest.TestCase):
+    _fig = None
+
+    @classmethod
+    def setUpClass(cls):
+        if _run_plot_test():
+            import matplotlib.pyplot as plt
+            cls.plt = plt
+            cls._fig = plt.figure()
+
+    @classmethod
+    def tearDownClass(cls):
+        if _run_plot_test():
+            cls.plt.show()
+
+    @unittest.skipUnless(_run_plot_test(), 'automated test')
+    def test_weiner_process(self):
+        """Plot Weiner Noise sampling"""
+        noise = misc.WienerNoise(shape=(1,), delta=1, dt=1, seed=0)
+        samples = [noise.sample() for _ in range(500)]
+        ax = self._fig.add_subplot(2, 1, 1)
+        ax.plot(samples)
+        ax.set_title('Weiner Process')
+
+    @unittest.skipUnless(_run_plot_test(), 'automated test')
+    def test_ou_process(self):
+        """Plot OU Noise sampling"""
+        theta, sigma = 0.15, 0.2
+        noise = misc.OUNoise(
+            shape=(1,), theta=theta, sigma=sigma, mu=0, seed=0)
+        samples = [noise.sample() for _ in range(500)]
+        ax = self._fig.add_subplot(2, 1, 2)
+        ax.plot(samples)
+        ax.set_title(
+            'Ornstein-Uhlenbeck Process: theta={}, sigma={}'
+            .format(theta, sigma)
+        )
