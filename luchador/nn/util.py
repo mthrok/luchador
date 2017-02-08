23f53b401967f40a7beb88ed99b0e3bc181502a3
moto <mthrok@gmail.com>
2017-02-08 11:55:04 -0800

    Add get_grad (#175)

--- luchador/nn/util.py
+++ luchador/nn/util.py
@@ -16,8 +16,10 @@ import luchador.nn
 
 from .model.sequential import make_sequential_model
 
-__all__ = ['make_model', 'get_model_config']
+__all__ = ['make_model', 'get_model_config', 'get_grad']
 
+
+###############################################################################
 _DATA_DIR = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), 'data')
 _LG = logging.getLogger(__name__)
@@ -108,3 +110,42 @@ def get_model_config(model_name, **parameters):
 
     model_text = StringIO.StringIO(model_text)
     return yaml.safe_load(model_text)
+
+
+###############################################################################
+def get_grad(var):
+    """Fetch gradient tensor corresponding to the given Variable
+
+    In optimizers, gradient tensors are registered in global list of tensors,
+    following the naming pattern `<scope>/<variable_name>_grad`.
+
+    This function automatically build such name from the given Variable and
+    the current scope.
+
+    To properly fetch the corresponding gradient Tensor, this function
+    must be called in the scope where gradient Tensor was defined.
+
+    Examples
+    --------
+    >>> from luchador import nn
+    >>> x = nn.get_variable(shape=(), name='x')
+    >>> # Variable x is registered with name 'x'
+    >>> y = x * x
+    >>> sgd = nn.optimizer.SGD(learning_rate=0.1)
+    >>> with nn.variable_scope('optimization'):
+    >>>    sgd.minimize(loss=y, wrt=x)
+    >>>    # dydx is registered with name '/optimization/x_grad'
+    >>>    dydx2 = nn.get_grad_tensor(x)
+    >>>    assert dydx1 is dydx2
+
+    Parameters
+    ----------
+    var : Variable
+        Variable object of which grad is retrieved.
+
+    Returns
+    -------
+    Tensor
+        Tensor object which is a gradient of given Variable
+    """
+    return luchador.nn.get_tensor('{}_grad'.format(var.name))
---
+++ tests/unit/nn/util_test.py
@@ -0,0 +1,37 @@
+"""Test nn.util module"""
+from __future__ import absolute_import
+
+from luchador import nn
+from tests.unit import fixture
+
+
+class UtilTest(fixture.TestCase):
+    """Test utility functions"""
+    def test_apply_gradient_directory(self):
+        """get_grad correctly fetches gradient Tensor from Variable"""
+        w_0 = 6
+        sgd = nn.optimizer.SGD(learning_rate=1.0)
+        with nn.variable_scope(self.get_scope()):
+            x = nn.Input(shape=(), name='x')
+            w1 = nn.get_variable(
+                name='w', shape=(),
+                initializer=nn.initializer.Constant(w_0),
+            )
+            y1 = w1 * x
+            sgd.minimize(y1, w1)
+            dy1dw1_1 = nn.get_tensor('{}_grad'.format(w1.name))
+            dy1dw1_2 = nn.get_grad(w1)
+
+            self.assertIs(dy1dw1_1, dy1dw1_2)
+
+        with nn.variable_scope('{}/2'.format(self.get_scope())):
+            w2 = nn.get_variable(
+                name='w', shape=(),
+                initializer=nn.initializer.Constant(w_0),
+            )
+            y2 = w2 * x
+            sgd.minimize(y2, w2)
+            dy2dw2_1 = nn.get_tensor('{}_grad'.format(w2.name))
+            dy2dw2_2 = nn.get_grad(w2)
+
+            self.assertIs(dy2dw2_1, dy2dw2_2)
