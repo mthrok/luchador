d8578d3f2d72787fe11ba18e7bb477e03fad232d
moto <mthrok@gmail.com>
2017-03-02 19:27:36 -0800

    Add exp/log (#242)

--- luchador/nn/core/backend/theano/misc.py
+++ luchador/nn/core/backend/theano/misc.py
@@ -10,7 +10,7 @@ from luchador.nn.core.base import wrapper as base_wrapper
 from .wrapper import Operation, Tensor, Variable
 
 __all__ = [
-    'build_sync_op', 'one_hot', 'maximum', 'minimum', 'abs',
+    'build_sync_op', 'one_hot', 'maximum', 'minimum', 'abs', 'exp', 'log',
     'clip_by_value', 'clip_by_norm',
 ]
 
@@ -91,6 +91,18 @@ def one_hot(var, n_classes, dtype=None, name=None):
 def abs(var, name=None):
     """Element-wise absolute value"""
     return var.__abs__(name=name)
+
+
+def exp(var, name=None):
+    """Returns exponential of the given variable"""
+    _tensor = T.exp(var.unwrap())
+    return Tensor(tensor=_tensor, shape=var.shape, name=name)
+
+
+def log(var, name=None):
+    """Returns exponential of the given variable"""
+    _tensor = T.log(var.unwrap())
+    return Tensor(tensor=_tensor, shape=var.shape, name=name)
 
 
 def maximum(var1, var2, name=None):
